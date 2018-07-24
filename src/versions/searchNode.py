b0c21a68d353b51d6ecf6fd85652d10778c80486
jmanuelrey <j.manuel.rey@udc.es>
2018-07-24 12:37:58 +0200

    Eliminado directorio versions

--- src/versions/searchNode.py
+++
@@ -1,9 +0,0 @@
-# AUTHOR: Juan Manuel Rey Escobar
-
-class SearchNode:
-    def __init__(self, state, parent, action, pathCost, depth):
-        self.state = state
-        self.parent = parent
-        self.action = action
-        self. pathCost = pathCost
-        self.depth = depth
