74b9433868137ea8ffc7335264c5f967fe62f1fc
jmanuelrey <j.manuel.rey@udc.es>
2018-07-29 22:32:14 +0200

    Added manifest and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(name='TFG',
+      version='0.1',
+      description='Demo Modules',
+      url='https://github.com/jmanuelrey/TFG',
+      author='Juan Manuel Rey Escobar',
+      author_email='j.manuel.rey@udc.es',
+      license='GPL-3.0',
+      packages=find_packages(),
+      include_packages_data=True,
+      test_suite='nose.collector',
+      test_require=['nose'],
+      install_requires=[])
